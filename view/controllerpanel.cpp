7459d1a7b4a3c3e2921bf7d029b67e533516f990
“黄灿” <huangcan@yintingkeji.com>
2026-05-03 18:18:03 +0800

    ControllerPanel onMessage 补充 setParam 调用

--- view/controllerpanel.cpp
+++ view/controllerpanel.cpp
@@ -1867,8 +1867,7 @@ void ControllerPanel::onMessage(IEvent *pEvent)
          // 设备数据（TCP / UDP / 串口 解析后投递）
          auto* pInit = static_cast<InitiativeMsgEvent*>(pEvent);
          const STParamInfo& param = pInit->getParamData();
-         qDebug() << "[UI] 收到设备数据 deviceId=" << param.unSourceID;
-//          updateDisplay(param);
+         setParam(param);
         break;
     }
 
